aa1e661305af611a39aaf01408c9bbd6499743b5
WZH <88584410+Mnsentinor@users.noreply.github.com>
2024-02-25 22:11:46 +0800

    Add files via upload

---
+++ Assets/Rigid_Bunny.cs
@@ -0,0 +1,207 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Rigid_Bunny : MonoBehaviour
+{
+    bool launched = false;
+    float dt = 0.015f;
+    Vector3 v = new Vector3(0, 0, 0);   // 速度
+    Vector3 w = new Vector3(0, 0, 0);   // 角速度
+
+    float mass;                                 // 质量
+    Matrix4x4 I_ref;                            // 参考惯性
+
+    float linear_decay = 0.999f;                // 用于速度衰减
+    float angular_decay = 0.98f;
+    float restitution = 0.5f;                 // 用于碰撞
+    float friction = 0.2f;                  
+
+    Vector3 gravity = new Vector3(0.0f, -9.8f, 0.0f);
+
+
+    // 初始化
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        float m = 1;
+        mass = 0;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            mass += m;
+            float diag = m * vertices[i].sqrMagnitude;//diag = mv^2
+            I_ref[0, 0] += diag;
+            I_ref[1, 1] += diag;
+            I_ref[2, 2] += diag;
+            I_ref[0, 0] -= m * vertices[i][0] * vertices[i][0];
+            I_ref[0, 1] -= m * vertices[i][0] * vertices[i][1];
+            I_ref[0, 2] -= m * vertices[i][0] * vertices[i][2];
+            I_ref[1, 0] -= m * vertices[i][1] * vertices[i][0];
+            I_ref[1, 1] -= m * vertices[i][1] * vertices[i][1];
+            I_ref[1, 2] -= m * vertices[i][1] * vertices[i][2];
+            I_ref[2, 0] -= m * vertices[i][2] * vertices[i][0];
+            I_ref[2, 1] -= m * vertices[i][2] * vertices[i][1];
+            I_ref[2, 2] -= m * vertices[i][2] * vertices[i][2];
+        }
+        I_ref[3, 3] = 1;
+    }
+
+    Matrix4x4 Get_Cross_Matrix(Vector3 a)//得到向量a的叉乘矩阵
+    {
+        Matrix4x4 A = Matrix4x4.zero;
+        A[0, 0] = 0;
+        A[0, 1] = -a[2];
+        A[0, 2] = a[1];
+        A[1, 0] = a[2];
+        A[1, 1] = 0;
+        A[1, 2] = -a[0];
+        A[2, 0] = -a[1];
+        A[2, 1] = a[0];
+        A[2, 2] = 0;
+        A[3, 3] = 1;
+        return A;
+    }
+
+    private Quaternion Add(Quaternion a, Quaternion b)
+    {
+        a.x += b.x;
+        a.y += b.y;
+        a.z += b.z;
+        a.w += b.w;
+        return a;
+    }
+
+    private Matrix4x4 Matrix_subtraction(Matrix4x4 a, Matrix4x4 b)
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                a[i, j] -= b[i, j];
+            }
+        }
+        return a;
+    }
+
+
+    private Matrix4x4 Matrix_miltiply_float(Matrix4x4 a, float b)
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                a[i, j] *= b;
+            }
+        }
+        return a;
+    }
+
+    // 在该函数中，通过碰撞产生的冲量更新v和w
+    // 平面 <P, N>
+    void Collision_Impulse(Vector3 P, Vector3 N)
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        Matrix4x4 R = Matrix4x4.Rotate(transform.rotation);  // 旋转矩阵
+        Vector3 T = transform.position;                     // 平移向量
+
+        Vector3 sum = new Vector3(0, 0, 0);                 
+        int collisionNum = 0;                               // 碰撞次数
+
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 r_i = vertices[i];
+            Vector3 Rri = R.MultiplyVector(r_i);
+            Vector3 x_i = T + Rri;
+            float d = Vector3.Dot(x_i - P, N);
+            if (d < 0.0f) // collision occur
+            {
+                Vector3 v_i = v + Vector3.Cross(w, Rri);
+                float v_N_size = Vector3.Dot(v_i, N);
+                // 检查速度
+                if (v_N_size < 0.0f)
+                {
+                    sum += r_i;
+                    collisionNum++;
+                }
+            }
+        }
+
+        if (collisionNum == 0) return;
+        Matrix4x4 I_rot = R * I_ref * R.transpose;
+        Matrix4x4 I_inverse = I_rot.inverse;      
+        Vector3 r_collision = sum / (float)collisionNum;                // 虚拟碰撞点（局部协调）
+        Vector3 Rr_collision = R.MultiplyVector(r_collision);
+        //Vector3 x_collision = T + Rr_collision;							 // 虚拟碰撞点（全局协调）
+        Vector3 v_collision = v + Vector3.Cross(w, Rr_collision);
+        
+        // 计算所需的 v_N
+        Vector3 v_N = Vector3.Dot(v_collision, N) * N;
+        Vector3 v_T = v_collision - v_N;
+        Vector3 v_N_new = -1.0f * restitution * v_N;
+        float a = Math.Max(1.0f - friction * (1.0f + restitution) * v_N.magnitude / v_T.magnitude, 0.0f);
+        Vector3 v_T_new = a * v_T;
+        Vector3 v_new = v_N_new + v_T_new;
+        
+        // 计算冲量 J
+        Matrix4x4 Rri_star = Get_Cross_Matrix(Rr_collision);
+        Matrix4x4 K = Matrix_subtraction(Matrix_miltiply_float(Matrix4x4.identity, 1.0f / mass),
+                                        Rri_star * I_inverse * Rri_star);
+        Vector3 J = K.inverse.MultiplyVector(v_new - v_collision);
+        
+        // 用冲量J更新v和w
+        v = v + 1.0f / mass * J;
+        w = w + I_inverse.MultiplyVector(Vector3.Cross(Rr_collision, J));
+    }
+
+    // 每帧调用一次更新
+    void Update()
+    {
+        //操作控制
+        if (Input.GetKey("r"))
+        {
+            // 返回初始状态
+            transform.position = new Vector3(0, 0.6f, 0);
+            transform.eulerAngles = new Vector3(80, 0, 0);
+            restitution = 0.5f;
+            launched = false;
+            Debug.Log("return to origin");
+        }
+        if (Input.GetKey("l"))
+        {
+            v = new Vector3(5, 2, 0);
+            w = new Vector3(0, 1, 0);
+            launched = true;
+        }
+
+        if (launched)
+        {
+            // Part I: 更新速度
+            v += dt * gravity;
+            v *= linear_decay;
+            w *= angular_decay;
+
+            // Part II: 碰撞冲量
+            Collision_Impulse(new Vector3(0, 0.01f, 0), new Vector3(0, 1, 0));
+            Collision_Impulse(new Vector3(2, 0, 0), new Vector3(-1, 0, 0));
+
+            // Part III: 更新位置和方向
+            Vector3 x_0 = transform.position;
+            Quaternion q_0 = transform.rotation;
+            //更新线性状态
+            Vector3 x = x_0 + dt * v;
+            //更新角度状态
+            Vector3 dw = 0.5f * dt * w;
+            Quaternion qw = new Quaternion(dw.x, dw.y, dw.z, 0.0f);
+            Quaternion q = Add(q_0, qw * q_0);
+
+            // Part IV: 指定给对象
+            transform.position = x;
+            transform.rotation = q;
+        }
+    }
+}
